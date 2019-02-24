387add9efab2dfafdd80f9af4ab1e7bfa880a796
Fynduk <ger-duk@mail.ru>
2019-02-24 18:03:49 +0300

    Added selection dir and files

--- pluginCalibrationAndDepth/widgetfilemodecalibration.h
+++ pluginCalibrationAndDepth/widgetfilemodecalibration.h
@@ -2,6 +2,7 @@
 #define WIDGETFILEMODECALIBRATION_H
 
 #include <QWidget>
+#include <QDir>
 
 namespace Ui {
 class WidgetFilemodeCalibration;
@@ -15,8 +16,21 @@ public:
     explicit WidgetFilemodeCalibration(QWidget *parent = nullptr);
     ~WidgetFilemodeCalibration();
 
+signals:
+    void emitVectorString(std::vector<std::string>);
+
+private slots:
+    void on_b_selectDirectory_released();
+
+    void on_b_selectFiles_released();
+
 private:
     Ui::WidgetFilemodeCalibration *ui;
+
+    QString m_path;
+    QStringList m_filesSorted; //left - right; l - r;...
+    QStringList m_files;
+    std::vector<std::string> m_pairs;
 };
 
 #endif // WIDGETFILEMODECALIBRATION_H
